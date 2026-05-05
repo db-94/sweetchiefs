a5a6e2669fee1f800248e68da2d2c2aefd93cf87
Martin <m@martincalvert.com>
2026-05-05 15:37:07 -0700

    Improve SEO: meta tags, Open Graph, Twitter Cards, sitemap, robots.txt (#1)

--- scripts/render-assets.js
+++ scripts/render-assets.js
@@ -6,6 +6,11 @@ const sh = require('shelljs');
 module.exports = function renderAssets() {
     const sourcePath = upath.resolve(upath.dirname(__filename), '../src/assets');
     const destPath = upath.resolve(upath.dirname(__filename), '../dist/.');
-    
-    sh.cp('-R', sourcePath, destPath)
+    sh.cp('-R', sourcePath, destPath);
+
+    const publicPath = upath.resolve(upath.dirname(__filename), '../src/public');
+    const distPath = upath.resolve(upath.dirname(__filename), '../dist');
+    sh.ls(publicPath).forEach(file => {
+        sh.cp(upath.join(publicPath, file), upath.join(distPath, file));
+    });
 };
